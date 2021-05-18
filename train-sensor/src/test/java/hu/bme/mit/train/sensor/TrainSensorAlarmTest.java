2eb0c69bea9214e01abb2e559ce4e7901b36ea8b
szondakata <katalin.szonda@gmail.com>
2021-05-18 12:51:45 +0200

    testcase fixed, jacoco added

---
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorAlarmTest.java
@@ -0,0 +1,55 @@
+package hu.bme.mit.train.sensor;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class TrainSensorAlarmTest {
+    TrainController mockTC;
+    TrainUser mockTU;
+    TrainSensor TS;
+
+    @Before
+    public void before() {
+        mockTC = mock(TrainController.class);
+        mockTU = mock(TrainUser.class);
+        TS = new TrainSensorImpl(mockTC, mockTU);
+    }
+
+    @Test
+    public void TestSpeedLimit() {
+        Assert.assertEquals(TS.getSpeedLimit(), 130);
+    }
+
+    @Test
+    public void TestNegativeSpeedLimit(){
+        TS.overrideSpeedLimit(-10);
+        verify(mockTU).setAlarmState(true);
+    }
+
+    @Test
+    public void TestTooBigSpeedLimit(){
+        TS.overrideSpeedLimit(543);
+        verify(mockTU).setAlarmState(true);
+    }
+
+    @Test
+    public void TestSmallerThanHalfSpeedLimit(){
+        when(mockTC.getReferenceSpeed()).thenReturn(130);
+
+        TS.overrideSpeedLimit(64);
+        verify(mockTU).setAlarmState(true);
+    }
+
+    @Test
+    public void TestInRangeLimit(){
+        TS.overrideSpeedLimit(65);
+        verify(mockTU).setAlarmState(false);
+    }
+}
